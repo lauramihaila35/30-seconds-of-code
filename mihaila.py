beae80d3716f49530383b1fa1019d4ccaa9d6f10
lauramihaila35 <lauramihaila35@outlook.com>
2025-07-04 11:01:44 +0300

    Create mihaila.py
    Signed-off-by: lauramihaila35 <lauramihaila35@outlook.com>

---
+++ mihaila.py
@@ -0,0 +1,37 @@
+from seleniumbase import SB
+import time
+import requests
+import sys
+import requests
+import os
+import random
+import subprocess
+from dataclasses import dataclass
+from typing import List, Optional
+
+import requests
+
+with SB(uc=True, test=True) as sb:
+
+    if True:
+        url = "https://kick.com/brutalles"
+        sb.uc_open_with_reconnect(url, 5)
+        sb.uc_gui_click_captcha()
+        sb.sleep(1)
+        sb.uc_gui_handle_captcha()
+        sb.sleep(1)
+        if sb.is_element_present('button:contains("Accept")'):
+            sb.uc_click('button:contains("Accept")', reconnect_time=4)
+        if sb.is_element_visible('#injected-channel-player'):
+            vvvvv = sb.get_new_driver(undetectable=True)
+            vvvvv.uc_open_with_reconnect(url, 5)
+            vvvvv.uc_gui_click_captcha()
+            vvvvv.uc_gui_handle_captcha()
+            sb.sleep(10)
+            if vvvvv.is_element_present('button:contains("Accept")'):
+                vvvvv.uc_click('button:contains("Accept")', reconnect_time=4)
+            while sb.is_element_visible('#injected-channel-player'):
+                sb.sleep(10)
+            sb.quit_extra_driver()
+    sb.sleep(1)
+
